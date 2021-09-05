537d16bdf507a3cb18952c5978a39b8bd2ad4dfc
Marlon Baptista de Quadros <marlonbquadros@gmail.com>
2021-09-05 11:35:54 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# CoopGameCourse
+
+Developed with Unreal Engine 4
---
+++ Source/CoopGameCourse/CoopGameCourse.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CoopGameCourse.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CoopGameCourse, "CoopGameCourse" );
